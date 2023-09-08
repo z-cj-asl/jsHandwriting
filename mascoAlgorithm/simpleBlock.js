883f7469de967ac0decec5f9284460fc1f0e0002
zcj <zcj@zcjdeMacBook-Pro.local>
2023-09-08 13:14:33 +0800

    fix: 分块算法测试

--- mascoAlgorithm/simpleBlock.js
+++ mascoAlgorithm/simpleBlock.js
@@ -1,9 +1,9 @@
-const mosaicify = (imageData) => {
-    
-    const iLen = imageData.height
-    const jLen = imageData.width
+function simpleMosaicify (frame)  {
+    let imageData = frame.data
+    const iLen = frame.width
+    const jLen = frame.height
     const blockSize = 10 // 马赛克块的大小
-    const mosaicifyData = new Array(iLen * jLen)
+    //const mosaicifyData = new Array(iLen * jLen)
   
     // 将每一块马赛克中的所有像素点的 rgba 都修改为块中第一个像素点的 rgba
     for (let i = 0; i < iLen; i += blockSize) {
@@ -19,15 +19,51 @@ const mosaicify = (imageData) => {
         for (let _i = i; _i < _iLen; _i++) {
           for (let _j = j; _j < _jLen; _j++) {
             const currentIndex = _i * 4 * jLen + _j * 4
-            mosaicifyData[currentIndex] = r
-            mosaicifyData[currentIndex + 1] = g
-            mosaicifyData[currentIndex + 2] = b
-            mosaicifyData[currentIndex + 3] = a
+            imageData[currentIndex] = r
+            imageData[currentIndex + 1] = g
+            imageData[currentIndex + 2] = b
+            imageData[currentIndex + 3] = a
           }
         }
       }
     }
 
-    return mosaicifyData
+    //return imageData
     
 }
+import config from './config'
+console.log(config)
+
+let Mcanvas = document.getElementById('canvas')
+let ctx = Mcanvas.getContext('2d') 
+// ctx.fillStyle = "green";
+// ctx.fillRect(20, 30, 50, 50)
+let masco = document.getElementById('masco')
+let mascoCtx = masco.getContext('2d') 
+
+let image = document.querySelector('img')
+
+ctx.drawImage(image, 0, 0, 250, 250)
+
+ctx.drawImage(image, 0, 0, 1080, 1080)
+
+let originalData = ctx.getImageData(0, 0, 250, 250)
+let originalDataTem = ctx.getImageData(0, 0, 1080, 1080)
+
+simpleMosaicify(originalData)
+mascoCtx.putImageData(originalData, 0, 0)
+
+simpleMosaicify(originalDataTem)
+mascoCtx.putImageData(originalDataTem, 300, 0)
+
+image.onload = ()=>{
+  console.log('aaa')
+  ctx.drawImage(image, 0, 0, 250, 250)
+  
+  let originalData = ctx.getImageData(0, 0, 250, 250)
+
+  let simpleBlockMoscoData = simpleMosaicify(originalData)
+
+  mascoCtx.drawImage(simpleBlockMoscoData, 0, 0, 250, 250)
+}
+        
